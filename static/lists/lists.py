f8a230212f3a6feda1b75a014bd57432bc057c24
Gild56 <gild56gmd@gmail.com>
2025-07-05 00:18:12 +0200

    preduction version fixes

--- static/lists/lists.py
+++ static/lists/lists.py
@@ -1,9 +1,5 @@
 import requests
 
-from static.lists.gild56_website_lists.challenges_list import challenges_list_top as challenges_list_top_offline
-from static.lists.gild56_website_lists.levels_list import levels_list_top as levels_list_top_offline
-from static.lists.gild56_website_lists.players import players as players_offline
-
 
 def load_list_from_py(url, variable_name):
     code = requests.get(url).text
@@ -14,22 +10,19 @@ def load_list_from_py(url, variable_name):
 
 def get_levels_list_top():
     levels_url = "https://raw.githubusercontent.com/Gild56/gild56_website_lists/main/levels_list.py"
-    # levels_list_top = load_list_from_py(levels_url, "levels_list_top")
-    levels_list_top = levels_list_top_offline
+    levels_list_top = load_list_from_py(levels_url, "levels_list_top")
     return levels_list_top
 
 
 def get_challenges_list_top():
     challenges_url = "https://raw.githubusercontent.com/Gild56/gild56_website_lists/main/challenges_list.py"
-    # challenges_list_top = load_list_from_py(challenges_url, "challenges_list_top")
-    challenges_list_top = challenges_list_top_offline
+    challenges_list_top = load_list_from_py(challenges_url, "challenges_list_top")
     return challenges_list_top
 
 
 def get_players():
     players_url = "https://raw.githubusercontent.com/Gild56/gild56_website_lists/main/players.py"
-    # players = load_list_from_py(players_url, "players")
-    players = players_offline
+    players = load_list_from_py(players_url, "players")
 
     levels_list_top = get_levels_list_top()
     challenges_list_top = get_challenges_list_top()
