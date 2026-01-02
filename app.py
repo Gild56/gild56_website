93d4101cd25a8cec720468bee27cf1b10d8a5939
Gild56 <gild56gmd@gmail.com>
2026-01-02 13:43:59 +0100

    port change

--- app.py
+++ app.py
@@ -513,5 +513,5 @@ def player_page(player: str):
         return redirect(url_for('error404'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # app.run(host="0.0.0.0", port=5000)
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
